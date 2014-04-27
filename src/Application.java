a136f95610fff2b0a7c7050c69ee698b596d9c94
Sumiran <srs2222@columbia.edu>
2014-04-27 17:34:30 -0400

    Step 1: Finding large datasets done

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,32 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 
 
 public class Application {
+	
+	public static String printArrayListToString(ArrayList<String> line) {
+		StringBuilder print = new StringBuilder();
+		for(String item : line) {
+			print.append("["+item+"] ");
+		}
+		return print.toString();
+	}
+	
+	public static String printMultipleArrayListsToString(ArrayList<ArrayList<String>> rows) {
+		StringBuilder print = new StringBuilder();
+		
+		for(ArrayList<String> itemSet : rows) {
+			print.append(printArrayListToString(itemSet));
+			print.append("\n");
+		}
+		
+		
+		return print.toString();
+	}
 	
 	static class Rule {
 		public ArrayList<String> LHS;
@@ -15,6 +37,8 @@ public class Application {
 		public Rule() {
 			LHS = new ArrayList<String>();
 			RHS = new ArrayList<String>();
+			supportFound = -1.0;
+			confidenceFound = -1.0;
 		}
 		
 		public String printToString() {
@@ -47,6 +71,10 @@ public class Application {
 			rows = new ArrayList<ArrayList<String>>();
 		}
 		
+		public String printToString() {
+			return printMultipleArrayListsToString(rows);
+		}
+		
 		public int findCount(ArrayList<String> items) {
 			int count = 0;
 			
@@ -74,8 +102,18 @@ public class Application {
 			return count;
 		}
 		
+		public int findCount(String item) {
+			ArrayList<String> wrapper = new ArrayList<String>();
+			wrapper.add(item);
+			return findCount(wrapper);
+		}
+		
 		public double findSupport(ArrayList<String> items) {
-			return (100.0*findCount(items))/rows.size();
+			return (1.0*findCount(items))/rows.size();
+		}
+		
+		public double findSupport(String item) {
+			return (1.0*findCount(item))/rows.size();
 		}
 		
 		public double findConfidence(Rule rule) {
@@ -95,16 +133,9 @@ public class Application {
 			LHSCount = findCount(rule.LHS);
 			RHSAndLHSCount = findCount(rule.RHS);
 			
-			return (100.0*RHSAndLHSCount)/LHSCount;
+			return (1.0*RHSAndLHSCount)/LHSCount;
 		}
 		
-	}
-	
-	public static ArrayList<Rule> aprioriMine(DataSet dataSet) {
-		ArrayList<Rule> rules = new ArrayList<Rule>();
-		
-		
-		return rules;
 	}
 	
 	public static DataSet dataSetFromFile(String path) {
@@ -114,15 +145,20 @@ public class Application {
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			String line;
 			while ((line = br.readLine()) != null) {
-			   String[] lineItems = line.split(",");
-			   
-			   ArrayList<String> dataSetRow = new ArrayList<String>();
-			   
-			   for(String item : lineItems) {
-				   dataSetRow.add(item.trim());
-			   }
-			   
-			   dataSet.rows.add(dataSetRow);
+				
+				if(line.trim().equals("")) {
+					continue;
+				}
+				
+				String[] lineItems = line.split(",");
+				   
+				ArrayList<String> dataSetRow = new ArrayList<String>();
+				 
+				for(String item : lineItems) {
+					dataSetRow.add(item.trim());
+				}
+				   
+				dataSet.rows.add(dataSetRow);
 			}
 			br.close();
 		} catch(Exception e) {
@@ -132,13 +168,157 @@ public class Application {
 		return dataSet;
 	}
 	
+	public static boolean itemsMatch(String item1, String item2) {
+		return (item1.trim().equalsIgnoreCase(item2.trim()));
+	}
+	
+	public static boolean canCombineItemsets(ArrayList<String> is1, ArrayList<String> is2) {
+		if(is1.size() != is2.size()) {
+			return false;
+		}
+		
+		int commonElements = 0;
+		
+		for(String item : is2) {
+			if(is1.contains(item)) {
+				commonElements += 1;
+			}
+		}
+		
+		return (commonElements == is1.size() - 1);
+	}
+	
+	public static ArrayList<String> combineItemsets(ArrayList<String> itemSet1, ArrayList<String> itemSet2) {
+		
+		if(!canCombineItemsets(itemSet1, itemSet2)) {
+			return null;
+		}
+		
+		ArrayList<String> combined = new ArrayList<String>();
+		for(String item : itemSet1) {
+			if(!combined.contains(item)) {
+				combined.add(item);
+			}
+		}
+		for(String item : itemSet2) {
+			if(!combined.contains(item)) {
+				combined.add(item);
+			}
+		}
+		
+		Collections.sort(combined);
+		return combined;
+	}
+	
+	public static boolean isSameItemset(ArrayList<String> set1, ArrayList<String> set2) {
+		if(set1.size() != set2.size()) {
+			return false;
+		}
+		
+		Collections.sort(set1);
+		Collections.sort(set2);
+		
+		for(int i=0;i<set1.size();i++) {
+			if(!set1.get(i).equals(set2.get(i))) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static boolean containsItemSet(ArrayList<ArrayList<String>> mainSet, ArrayList<String> newItemset) {
+		if(mainSet.size() == 0) {
+			return false;
+		}
+		for(ArrayList<String> i : mainSet) {
+			if(isSameItemset(i, newItemset)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static ArrayList<ArrayList<String>> c_KPlusOneFromL_K(ArrayList<ArrayList<String>> itemSets) {
+		ArrayList<ArrayList<String>> combinations = new ArrayList<ArrayList<String>>();
+		for(int i=0;i<itemSets.size() - 1;i++) {
+			for(int j=i+1;j<itemSets.size();j++) {
+				ArrayList<String> combo = combineItemsets(itemSets.get(i), itemSets.get(j)); 
+				if(combo != null) {
+					if(!containsItemSet(combinations, combo)) {
+						combinations.add(combo);
+					}
+				}
+			}
+		}
+		return combinations;
+	}
+	
+	public static ArrayList<Rule> aprioriMine(DataSet dataSet, double minSupport, double minConfidence) {
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+		
+		int k = 1;
+		ArrayList<ArrayList<String>> large_k_itemsets = new ArrayList<ArrayList<String>>();
+		HashSet<String> uniqueItems = new HashSet<String>();
+		
+		for(ArrayList<String> row : dataSet.rows) {
+			for(String item : row) {
+				if(!uniqueItems.contains(item)) {
+					uniqueItems.add(item);
+				}
+			}
+		}
+		
+		for(String item : uniqueItems) {
+			if(dataSet.findSupport(item) >= minSupport) {
+				ArrayList<String> initial = new ArrayList<String>();
+				initial.add(item);
+				large_k_itemsets.add(initial);
+			}
+		}
+		
+		//printMultipleArrayListsToString(large_k_itemsets);
+		
+		while(true) {
+			ArrayList<ArrayList<String>> newItemsets = new ArrayList<ArrayList<String>>();
+			
+			ArrayList<ArrayList<String>> candidateSets = c_KPlusOneFromL_K(large_k_itemsets);
+			for(ArrayList<String> itemSet : candidateSets) {
+				if(dataSet.findSupport(itemSet) >= minSupport) {
+					newItemsets.add(itemSet);
+				}
+			}
+			
+			if(newItemsets.size() == 0) {
+				break;
+			}
+			
+			k += 1;
+			large_k_itemsets = newItemsets;
+		}
+		
+		System.out.println(printMultipleArrayListsToString(large_k_itemsets));
+		
+		return rules;
+	}
+	
 	public static void main(String args[]) {
-		String[] myArg = {"MyFile.csv", "0.3", "0.5"};
+		String[] myArg = {"D:\\EclipseWorkspace\\AprioriMiner\\INTEGRATED-DATASET.csv", "0.1", "0.5"};
 		args = myArg;
 		
-		String filePath = args[0];
-		double minSupport = Double.parseDouble(args[1]);
-		double minConfidence = Double.parseDouble(args[2]);
+		String filePath;
+		double minSupport;
+		double minConfidence;
+		
+		try {
+			filePath = args[0];
+			minSupport = Double.parseDouble(args[1]);
+			minConfidence = Double.parseDouble(args[2]);
+		} catch(Exception e) {
+			System.out.println("Unable to parse arguments: ");
+			e.printStackTrace();
+			return;
+		}
 		
 		System.out.println("Reading file: 				      "+filePath);
 		System.out.println("Finding rules with minSupport:    "+minSupport);
@@ -146,10 +326,12 @@ public class Application {
 		
 		DataSet myDataSet = dataSetFromFile(filePath);
 		
-		System.out.println("Mining frequent itemsets and rules...");
-		ArrayList<Rule> rulesFound = aprioriMine(myDataSet);
+		//System.out.println(myDataSet.printToString());
 		
-		System.out.println("\nFound the following rules: ");
+		System.out.println("Mining frequent itemsets and rules...");
+		ArrayList<Rule> rulesFound = aprioriMine(myDataSet, minSupport, minConfidence);
+		
+		System.out.println("\nFound the following "+rulesFound.size()+" rules: ");
 		for(Rule r : rulesFound) {
 			System.out.println(r.printToString());
 		}
